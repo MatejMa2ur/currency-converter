59ce98e6b927b689817555cbd50ceff9b630851b
MatejMa2ur <matejmazur@outlook.com>
2024-04-21 22:57:18 +0200

    Remove unused usings, add Serilog in Program.cs
    In this commit, all the unused namespaces in the Program.cs file within the CurrencyConverter.Api project were removed to clean up the code. In addition, the Serilog namespace was also added to enhance logging capabilities.

--- CurrencyConverter.Api/Program.cs
+++ CurrencyConverter.Api/Program.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Linq;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
